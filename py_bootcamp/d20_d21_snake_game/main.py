19a75d951da8f4804180b413f26e80e4db3c9c6b
anandushivakumar <anandu.shivakumar@gmail.com>
2025-08-29 15:52:58 +0400

    Completed d20/21 snake game

--- py_bootcamp/d20_d21_snake_game/main.py
+++ py_bootcamp/d20_d21_snake_game/main.py
@@ -1,5 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.tracer(0)
 screen.title("Snake Game")
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,5 +25,24 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # detect collision with food
+    if snake.segments[0].distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_scoreboard()
+    
+    # detect collision with wall
+    if snake.segments[0].xcor() > 280 or snake.segments[0].xcor() < -280 or snake.segments[0].ycor() > 280 or snake.segments[0].ycor() < -280:
+        game_is_on = False
+        scoreboard.goto(0, 0)
+        scoreboard.write("Game Over!", align = "center", font = ("Arial", 24, "normal"))
+    
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.segments[0].distance(segment) < 10:
+            game_is_on = False
+            scoreboard.goto(0, 0)
+            scoreboard.write("Game Over!", align = "center", font = ("Arial", 24, "normal"))
 
 screen.exitonclick()
