d4f763f2ff5069e991f5aab3b5a48f8557e92fbf
anandushivakumar <anandu.shivakumar@gmail.com>
2025-09-04 23:57:36 +0400

    d26 nato alphabet completed

--- py_bootcamp/d26_nato/main.py
+++ py_bootcamp/d26_nato/main.py
@@ -7,5 +7,6 @@ alphabet_dict = {row.letter:row.code for (index, row) in alphabet_df.iterrows()}
 # ask user for word
 user_input = input("Enter a word: ").upper()
 
+
 output = [alphabet_dict[l] for l in user_input]
 print(output)
