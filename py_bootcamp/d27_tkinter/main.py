7f29a598b8f021c31517be2bb378aa6ac0b8da68
anandushivakumar <anandu.shivakumar@gmail.com>
2025-09-06 17:38:56 +0400

    d7 tkinter intro complete

--- py_bootcamp/d27_tkinter/main.py
+++ py_bootcamp/d27_tkinter/main.py
@@ -1,24 +1,41 @@
+# miles to kilometers converter project
+
 from tkinter import *
 
 window = Tk()
-window.title("Test program")
-window.minsize(width=500, height=300)
+window.title("Miles to Kilometers Converter")
+window.minsize(width=200, height=100)
 
-# label
-label = Label(text = "I am a label", font=("Arial", 24))
-label.pack() # to show the label on the screen
+# entry widget for miles input
+miles = Entry(window, width = 10)
 
-# button
-def button_click():
-    print("I got clicked")
-    new_text = input.get()
-    label.config(text = new_text)
+# label widget for miles, "is equal to", result and "kilometers"
+miles_label = Label(window, text = "Miles")
+is_equal_to = Label(window, text = "is equal to")
+km_result_label = Label(window, text = "0")
+km_label = Label(window, text = "Kilometers")
 
-button = Button(text = "Click me", command=button_click)
-button.pack()
+# button for conversion
+calculate = Button(window, text = "calculate")
 
-# entry
-input = Entry(width=10)
-input.pack()
+# place the widgets according to grid
+miles.grid(row = 0, column = 1)
+miles_label.grid(row = 0, column = 2)
+is_equal_to.grid(row = 1, column = 0)
+km_result_label.grid(row = 1, column = 1)
+km_label.grid(row = 1, column = 2)
+calculate.grid(row = 3, column = 1)
+
+# config
+window.config(padx = 20, pady = 20)
+
+# function for conversion
+def conversion():
+    mi = float(miles.get())
+    km = mi * 1.609 
+    km_result_label.config(text = f"{km:.2f}")
+
+# bind button
+calculate.config(command=conversion)
 
 window.mainloop()
