d60ac03a13317c24504ab095e168c4a3e8798c4f
anandushivakumar <anandu.shivakumar@gmail.com>
2025-09-12 23:27:25 +0400

    d35 weather api incomplete

---
+++ py_bootcamp/d35_api_part_3/rain_alert.py
@@ -0,0 +1,31 @@
+import requests
+
+API_ID = "129b616b2748b4b2f1896c37603ff518"
+MY_LAT = 25.20
+MY_LON = 55.29
+
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LON,
+    "appid": API_ID,
+    "cnt": 4
+}
+
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params = parameters)
+response.raise_for_status()
+data = response.json()
+#print(data)
+
+# check weather data for the next 12 hours and primt temp and ask to bring umbrella if temperature is above 35C
+for entry in data["list"]:
+    max_temp = entry["main"]["temp_max"]
+    max_temp_c = max_temp - 273.15
+    weather_id = entry["weather"][0]["id"]
+    print(f"Max temperature: {max_temp_c} C")
+    print(f"Weather ID: {weather_id}")
+    if weather_id == 800 and max_temp_c > 30:
+        print("Bring an umbrella")
+        break
+    else:
+        print("Don't bring an umbrella")
+        break
